e50de7e2d0bf2e5236b935a123e612c238312021
HadisYJ <hadis.yadollahi@gmail.com>
2024-08-28 11:01:12 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+let iconMapping = {
+  "clear-sky-day": "Images/Sun day.svg",
+  "thunderstorm-day": "Images/Thunder day.svg",
+  "few-clouds-day": "Images/few clouds day.svg",
+  "shower-rain-day": "Images/rain day.svg",
+  "rain-day": "Images/sun rain day.svg",
+  "broken-clouds-day": "Images/broken clouds day.svg",
+  "overcast-clouds-day": "Images/broken clouds day.svg",
+  "scattered-clouds-day": "Images/few clouds day.svg",
+  "snow-day": "Images/snow day.svg",
+  "mist-day": "Images/mist day.svg",
+  "clear-sky-night": "Images/Moon.svg",
+  "thunderstorm-night": "Images/Thunder night.svg",
+  "few-clouds-night": "Images/few clouds night.svg",
+  "shower-rain-night": "Images/rain night.svg",
+  "rain-night": "Images/rain night.svg",
+  "broken-clouds-night": "Images/few clouds night.svg",
+  "overcast-clouds-night": "Images/Moon clouds night.svg",
+  "scattered-clouds-night": "Images/Moon clouds night.svg",
+  "snow-night": "Images/snow night.svg",
+  "mist-night": "Images/mist night.svg",
+};
+
 function updateWeatherInfo(response) {
   let currentTemperature = document.querySelector(".current-temperature");
   let h1 = document.querySelector("h1");
@@ -7,28 +30,6 @@ function updateWeatherInfo(response) {
   let timeElement = document.querySelector(".time");
   let date = new Date(response.data.time * 1000);
 
-  let iconMapping = {
-    "clear-sky-day": "Images/Sun day.svg",
-    "thunderstorm-day": "Images/Thunder day.svg",
-    "few-clouds-day": "Images/few clouds day.svg",
-    "shower-rain-day": "Images/rain day.svg",
-    "rain-day": "Images/sun rain day.svg",
-    "broken-clouds-day": "Images/broken clouds day.svg",
-    "overcast-clouds-day": "Images/broken clouds day.svg",
-    "scattered-clouds-day": "Images/few clouds day.svg",
-    "snow-day": "Images/snow day.svg",
-    "mist-day": "Images/mist day.svg",
-    "clear-sky-night": "Images/Moon.svg",
-    "thunderstorm-night": "Images/Thunder night.svg",
-    "few-clouds-night": "Images/few clouds night.svg",
-    "shower-rain-night": "Images/rain night.svg",
-    "rain-night": "Images/rain night.svg",
-    "broken-clouds-night": "Images/few clouds night.svg",
-    "overcast-clouds-night": "Images/Moon clouds night.svg",
-    "scattered-clouds-night": "Images/Moon clouds night.svg",
-    "snow-night": "Images/snow night.svg",
-    "mist-night": "Images/mist night.svg",
-  };
   let weatherCondition = response.data.condition.icon;
   let iconPath = iconMapping[weatherCondition];
 
@@ -39,6 +40,8 @@ function updateWeatherInfo(response) {
   humidity.innerHTML = response.data.temperature.humidity;
   wind.innerHTML = response.data.wind.speed;
   icon.innerHTML = `<img src="${iconPath}" alt="${weatherCondition} icon"/>`;
+
+  getForecast(response.data.city);
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -63,7 +66,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "fe7b5aa15d53a1t7b432406eac34f4oa";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(updateWeatherInfo);
 }
 
@@ -72,6 +75,51 @@ function searchCitySubmit(event) {
   let inputCity = document.querySelector("#citySearchField");
   searchCity(inputCity.value);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "fe7b5aa15d53a1t7b432406eac34f4oa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 6) {
+      let weatherCondition = day.condition.icon;
+      let iconPath = iconMapping[weatherCondition];
+      forecastHtml += `
+        <div class="forcastWeather">
+          <div class="forcastDay">${formatDay(day.time)}</div>
+          <div>
+            <img src="${iconPath}" class="forcastIcon" alt="${weatherCondition} icon" />
+          </div>
+          <div class="forcastTemps">
+            <span class="forcastTempMax">${Math.round(
+              day.temperature.maximum
+            )}°</span>
+            -
+            <span class="forcastTempMin">${Math.round(
+              day.temperature.minimum
+            )}°</span>
+          </div>
+        </div>
+      `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let submitSearch = document.querySelector("#searchButton");
 submitSearch.addEventListener("click", searchCitySubmit);
 
